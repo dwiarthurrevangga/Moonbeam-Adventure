407e321d14820c822d639e87ec43389ee1395526
Hanz <61815053+Mad-Moon39@users.noreply.github.com>
2024-05-05 20:16:21 +0700

    Script entity, utility, , tilemap

---
+++ Script/tilemap.py
@@ -0,0 +1,44 @@
+import pygame
+
+
+NEIGHBOR_OFFSETS = [(-1, 0), (-1, 1), (0, -1), (1, -1), (1, 0), (0, 0), (-1, 1), (0, 1), (1, 1)]
+PHYSICS_TILES = {'stone'}
+
+class TileMap :
+    def __init__(self, game, tile_size = 16):
+        self.game = game
+        self.tile_size = tile_size
+        self.tilemap = {}
+        self.offgrid_tiles = []
+
+        for i in range(10):
+            #self.tilemap[str(3+i) + ';10'] = {'type' : 'itembox', 'variant' : 1, 'pos' : (3 + i, 10)}
+            self.tilemap[';10i' + str(5+i)] = {'type' : 'stone', 'variant' : 1, 'pos' : (10, 5 + i)}
+            self.tilemap[str(3+i) + ';10'] = {'type' : 'stone', 'variant' : 1, 'pos' : (3 + i, 10)}
+    
+    def tiles_around(self, pos):
+        tiles = []
+        tile_loc = (int(pos[0]//self.tile_size), int(pos[1]//self.tile_size))
+        for offset in NEIGHBOR_OFFSETS :
+            check_loc = str(tile_loc[0] + offset[0]) + ';' + str(tile_loc[1] + offset[1])
+            if check_loc in self.tilemap:
+                tiles.append(self.tilemap[check_loc])
+        return tiles
+    
+    def physics_rect_around(self, pos):
+        rects = []
+        for tile in self.tiles_around(pos):
+            if tile['type'] in PHYSICS_TILES:
+                rects.append(pygame.Rect(tile['pos'][0]*self.tile_size, tile['pos'][1]*self.tile_size, self.tile_size, self.tile_size))
+        return rects
+
+    def render(self, surf):
+
+        for tile in self.offgrid_tiles:
+            surf.blit(self.game.assets[tile['type']][tile['variant']], tile['pos'][0])
+
+        for loc in  self.tilemap:
+            tile = self.tilemap[loc]
+            surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size))
+
+        
